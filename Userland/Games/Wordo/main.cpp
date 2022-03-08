fbf54e0dee6995f6e46d6b567717a4811c9da67f
Andrew Clunis <andrew@orospakr.ca>
2022-03-07 22:29:43 -0500

    create skeleton of wordo game

---
+++ Userland/Games/Wordo/main.cpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2020, Andrew Clunis <andrew@orospakr.ca>
+ * Copyright (c) 2021, the SerenityOS developers.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibGUI/Application.h>
+#include <LibMain/Main.h>
+#include <LibConfig/Client.h>
+#include <LibCore/System.h>
+#include <LibGUI/Icon.h>
+#include <LibGUI/BoxLayout.h>
+#include <LibGUI/Window.h>
+#include <stdio.h>
+
+ErrorOr<int> serenity_main(Main::Arguments arguments) {
+    TRY(Core::System::pledge("stdio recvfd sendfd rpath unix"));
+
+    auto app = TRY(GUI::Application::try_create(arguments));
+    auto app_icon = TRY(GUI::Icon::try_create_default_icon("app-wordo"));
+    auto window = TRY(GUI::Window::try_create());
+
+    window->set_title("Wordo");   
+
+    window->set_icon(app_icon.bitmap_for_size(16));
+    // window->resize(300, 300);
+    window->show();
+
+    // auto main_widget = TRY(window->try_set_main_widget<GUI::Widget>());
+    // (void)TRY(main_widget->try_set_layout<GUI::VerticalBoxLayout>());
+    // main_widget->set_fill_with_background_color(true);
+
+    return app->exec();
+}
